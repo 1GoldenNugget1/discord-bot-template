ba6a82a10a2c0e588000828ea40f657409811639
1GoldenNugget1 <76992055+1GoldenNugget1@users.noreply.github.com>
2025-03-31 18:57:59 +0200

    general template added

---
+++ bot.mjs
@@ -0,0 +1,44 @@
+//common  packages used 
+import { GatewayIntentBit, EmbedBuilder, Message, GatewayIntentBits } from "discord.js";
+import DiscordJS from 'discord.js';
+import fetch from 'node-fetch';
+
+dotenv.config();
+// setting up  intents mostly used by my projects 
+const client = new DiscordJS.Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers
+    ]
+    
+});
+
+client.on('ready', () =>{
+    console.log('bot is up and running')
+});
+//prefix setup
+const prefix = '%'
+const prefixLength = 1
+//setting up new discordjs  client itself
+client.on('messageCreate', async (message) =>{
+    if (message.author.bot) return; //ignore bot messages
+    if (!message.content.startsWith(prefix)) return; //ignore messages that  do not start with given prefix 
+    const args = message.content.slice(prefixLength).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+    /* creating command switch in order to prevent  memory leaks and 
+    exceeding event listeners limit */
+     switch (command) {
+        //enter commands cases here 
+
+     };
+
+
+
+
+});
+client.login(process.env.token)
+
+
+
+
---
+++ pkg-install.ps1
@@ -0,0 +1,4 @@
+npm i discord.js@latest 
+npm i dotenv
+npm i pretty-ms
+npm i nodemon
